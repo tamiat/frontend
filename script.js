e6e85048f1f220a7a4000f7c8465d870dfa1a8b0
Mohamed Sayed <mohamedsayedaz18@gmail.com>
2021-08-23 14:30:29 +0200

    Fix FetchAPI function

--- script.js
+++ script.js
@@ -1,19 +1,11 @@
-function fetchAPI(){
-    let apiUrl = "http:\\localhost:8080\\" + document.getElementById("content-id").value;
-    fetch(apiUrl,{
-        method:'POST',
-        headers:{
-            'content-type':'application/json'
-         }
-    }).then(res=>{
-        if(res.ok){
-            //console.log(res.json);
-            const data = JSON.parse(res.json);
-            document.getElementById("lbl-id").value = data.id;
-            document.getElementById("lbl-title").value = data.title;
-            document.getElementById("lbl-details").value = data.details;
-        }else{
-            alert("Not Found")
-        }}
-    );
-}
+const userAction = async () => {
+    //this a apiurl for just test
+    const apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=London&appid=bae4607475c5b1f680537d0dfbfb4994";// + document.getElementById("content-id").value;
+    const response = await fetch(apiUrl);
+    const data = await response.json(); //extract JSON from the http response
+    // do something with data
+    console.log(data);
+    document.getElementById("lbl_id").innerText = data.id;
+    document.getElementById("lbl_title").innerText = data.title;
+    document.getElementById("lbl_details").innerText = data.details;
+  }
