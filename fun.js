9607005dd9c7df9e1aca4f76236af032883133ee
Mohamed Sayed <mohamedsayedaz18@gmail.com>
2021-09-02 15:00:25 +0200

    Update Content Type Section

--- fun.js
+++ fun.js
@@ -1,17 +1,18 @@
+const host = "http://localhost:8080/api/";
 const deleteContentType = async () => {
     //this a apiurl for just test
-    const apiUrl = "http://localhost:8080/api/v1/contentType?id=" + document.getElementById("d_id").value;
+    const apiUrl = host + "v1/contentType?id=" + document.getElementById("d_id").value;
     const response = await fetch(apiUrl,{
     	method:'DELETE',
     	headers:{
-    		 'Content-Type': 'application/json';
+    		 'Content-Type': 'application/json'
     	}
     });
     const data = await response.json(); //extract JSON from the http response
   }
 const createContentType = async () => {
     //this a apiurl for just test
-    const apiUrl = "http://localhost:8080/api/v1/contentType";
+    const apiUrl = host + "v1/contentType";
     
     const name = document.getElementById("c_name").value;
     const id = document.getElementById("c_id").value;
@@ -22,7 +23,55 @@ const createContentType = async () => {
     	method:'POST',
     	body:myBody,
     	headers:{
-    		'Content-Type': 'application/json';
+    		'Content-Type': 'application/json'
+    	}
+    });
+    const data = await response.json(); //extract JSON from the http response
+  }
+const updateCol = async () => {
+    //this a apiurl for just test
+    const apiUrl = host + "v1/contentType/renamecol?id=" + document.getElementById("u_id");
+    
+    const name = document.getElementById("u_name").value;
+    
+    let myBody="{\n\"newcol\" : \"" + name + "\"\n}";
+    const response = await fetch(apiUrl,{
+    	method:'PUT',
+    	body:myBody,
+    	headers:{
+    		'Content-Type': 'application/json'
+    	}
+    });
+    const data = await response.json(); //extract JSON from the http response
+  }
+const addCol = async () => {
+    //this a apiurl for just test
+    const apiUrl = host + "v1/contentType/addcol?id=" + document.getElementById("u_id");
+    
+    const name = document.getElementById("u_name").value;
+    
+    let myBody="{\n\"newCol\" : \"" + name + "\"\n}";
+    const response = await fetch(apiUrl,{
+    	method:'PUT',
+    	body:myBody,
+    	headers:{
+    		'Content-Type': 'application/json'
+    	}
+    });
+    const data = await response.json(); //extract JSON from the http response
+  }
+const deleteCol = async () => {
+    //this a apiurl for just test
+    const apiUrl = host + "v1/contentType/delcol?id=" + document.getElementById("u_id");
+    
+    const name = document.getElementById("u_name").value;
+    
+    let myBody="{\n\"column name\" : \"" + name + "\"\n}";
+    const response = await fetch(apiUrl,{
+    	method:'PUT',
+    	body:myBody,
+    	headers:{
+    		'Content-Type': 'application/json'
     	}
     });
     const data = await response.json(); //extract JSON from the http response
